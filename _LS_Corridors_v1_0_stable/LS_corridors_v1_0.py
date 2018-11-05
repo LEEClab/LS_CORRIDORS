4768d7e7aa6389661d04556541c2ec86bd8f3b20
John Wesley Ribeiro <jw.ribeiro.rc@gmail.com>
2018-11-05 16:40:16 -0200

    uma nova solucao, com uma funcao global, ainda para static text

--- _LS_Corridors_v1_0_stable/LS_corridors_v1_0.py
+++ _LS_Corridors_v1_0_stable/LS_corridors_v1_0.py
@@ -156,24 +156,16 @@ def combine_st(st_map):
   return patchid_list_output
    
 
-#----------------------------------------------------------------------------------
-# Corridors is the main class, in which the software is initialized and runs
-class StaticText(object):
-  def __init__(self, parent, id):
-    pass
-    
-  def main(self):
-    #__________________________________________________________________________________________                
-    self.quote = wx.StaticText(self, id=-1, label="Import Maps:", pos=wx.Point(20,95))    
-    font = wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD)
-    self.quote.SetForegroundColour("red")
-    self.quote.SetFont(font)    
-  
+def  StaticText(self):
+  self.quote = wx.StaticText(self, id=-1, label="Import Maps:", pos=wx.Point(20,95))    
+  font = wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD)
+  self.quote.SetForegroundColour("red")
+  self.quote.SetFont(font)   
 
-class Corridors(wx.Panel,StaticText):
+class Corridors(wx.Panel):
     def __init__(self, parent, id):
         wx.Panel.__init__(self, parent, id)
-        StaticText.__init__(self, parent, id)
+        
         
         # Takes the current mapset and looks for maps only inside it
         self.current_mapset = grass.read_command('g.mapset', flags = 'p').replace('\n','').replace('\r','')
@@ -406,7 +398,7 @@ class Corridors(wx.Panel,StaticText):
         #self.quote.SetFont(font)
         
         #__________________________________________________________________________________________ 
-        StaticText.main(self)
+        StaticText(self)
         #self.quote = wx.StaticText(self, id=-1, label="Import Maps:", pos=wx.Point(20,95))    
         #font = wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD)
         #self.quote.SetForegroundColour("red")
